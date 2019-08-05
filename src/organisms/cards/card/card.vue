daadce4dbba14ca07b334d6ad28ad7113ccf98ab
Simon Erlandsen <simon.erlandsen@gmail.com>
2019-08-05 14:26:06 +0200

    completed card component.

--- src/organisms/cards/card/card.vue
+++ src/organisms/cards/card/card.vue
@@ -1,48 +1,111 @@
 <template>
-  <div class="osg-card">
-    <a class="osg-card__wrapper" :href="link.url" :title="link.title">
-      <div v-if="image" class="osg-card__image-container">
-        <figure class="osg-figure osg-v-default ">
-          <picture>
-            <img :src="image.url" :alt="image.alt" />
-          </picture>
-        </figure>
+  <div
+    class="osg-card"
+    :class="[
+      { 'osg-card--with-state': stateText },
+      { 'osg-card--animate': hasAnimation }
+    ]"
+  >
+    <a
+      class="osg-card__wrapper"
+      :is="url ? 'a' : 'div'"
+      :href="url"
+      :title="urlTitle"
+    >
+      <div v-if="imageUrl" class="osg-card__image-container">
+        <osg-vue-figure
+          :url="imageUrl"
+          :url-mobile="imageUrlMobile"
+          :url-tablet="imageUrlTablet"
+          :url-desktop="imageUrlDesktop"
+          :sr-description="imageSrDescription"
+        />
+        <div
+          v-if="stateText"
+          class="osg-card__state-text"
+        >
+          {{ stateText }}
+        </div>
       </div>
       <osg-vue-content-box>
         <slot />
+        <a
+          v-if="searchResult && searchResultUrl"
+          :href="searchResultUrl"
+          class="osg-card__search-result"
+        >
+          {{ searchResult }}
+        </a>
       </osg-vue-content-box>
     </a>
   </div>
 </template>
 
 <script>
-import OsgVueContentBox from '../../../molecules/content_display/content_box/content_box'
+  import OsgVueContentBox from '../../../molecules/content_display/content_box/content_box'
+  import OsgVueFigure from '../../../atoms/decorators/figure/figure.vue'
 
-export default {
-  name: 'OsgCard',
+  export default {
+    name: 'OsgCard',
 
-  components: {
-    OsgVueContentBox
-  },
-
-  props: {
-    isDownload: {
-      type: Boolean,
-      default: false
+    components: {
+      OsgVueContentBox,
+      OsgVueFigure
     },
-    image: {
-      type: Object
-    },
-    link: {
-      type: Object,
-      required: true
+
+    props: {
+      url: {
+        type: String,
+        default: null
+      },
+
+      urlTitle: {
+        type: String,
+        default: null
+      },
+
+      imageUrl: {
+        type: String
+      },
+
+      imageUrlMobile: {
+        type: String
+      },
+
+      imageUrlTablet: {
+        type: String
+      },
+
+      imageUrlDesktop: {
+        type: String
+      },
+
+      imageSrDescription: {
+        type: String
+      },
+
+      stateText: {
+        type: String,
+      },
+
+      hasAnimation: {
+        type: Boolean,
+        default: false
+      },
+
+      searchResult: {
+        type: String
+      },
+
+      searchResultUrl: {
+        type: String
+      }
     }
   }
-}
 </script>
 
 <style lang="sass">
   @import "~styleguide/src/assets/sass/resources"
-  @import "~styleguide/src/assets/sass/common"
+  @import "~styleguide/src/assets/sass/common/utils"
   @import "~styleguide/src/organisms/cards/card/card"
 </style>
