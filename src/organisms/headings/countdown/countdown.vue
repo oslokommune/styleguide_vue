6a66c527966ae51198cabceb06c3970a02a484fa
Simon Erlandsen <simon.erlandsen@gmail.com>
2019-08-08 14:03:53 +0200

    using component vue-countdown.

--- src/organisms/headings/countdown/countdown.vue
+++ src/organisms/headings/countdown/countdown.vue
@@ -1,29 +1,38 @@
 <template>
-  <div class="osg-countdown ">
+  <div class="osg-countdown">
     <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-    <div class="osg-countdown__timing">
-      <div class="osg-countdown__days">
-        <span>30</span>
-        <span>Morbi</span>
-      </div>
-      <div class="osg-countdown__hours">
-        <span>30</span>
-        <span>Velit</span>
-      </div>
-      <div class="osg-countdown__minutes">
-        <span>30</span>
-        <span>Consequat</span>
-      </div>
-      <div class="osg-countdown__seconds">
-        <span>30</span>
-        <span>Feugiat</span>
-      </div>
-    </div>
+    <vue-countdown :time="2 * 24 * 60 * 60 * 1000">
+      <template slot-scope="props">
+        <div class="osg-countdown__timing">
+          <div class="osg-countdown__days">
+            <span>{{ props.days }}</span>
+            <span>Dager</span>
+          </div>
+          <div class="osg-countdown__hours">
+            <span>{{ props.hours }}</span>
+            <span>Timer</span>
+          </div>
+          <div class="osg-countdown__minutes">
+            <span>{{ props.minutes }}</span>
+            <span>Minutter</span>
+          </div>
+          <div class="osg-countdown__seconds">
+            <span>{{ props.seconds }}</span>
+            <span>Sekunder</span>
+          </div>
+        </div>
+      </template>
+    </vue-countdown>
   </div>
 </template>
 
 <script>
+  import VueCountdown from '@chenfengyuan/vue-countdown'
   export default {
+    components: {
+      VueCountdown
+    },
+
     props: {
       countDownTo: {
         type: String
