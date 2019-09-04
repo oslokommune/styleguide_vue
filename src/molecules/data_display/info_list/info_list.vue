13d1c21a672bea9a94d31d3f5330933d7c5e7bb9
Mimi Nguyen <mimin@netcompany.com>
2019-09-04 16:21:09 +0200

    Added InfoList and Event components

---
+++ src/molecules/data_display/info_list/info_list.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="osg-info-list">
+    <ul>
+      <li v-if="calendarTitle || calendarText">
+        <div class="osg-icon osg-v-default osg-u-icon-calendar">
+          <span class="osg-sr-only">Calendar</span>
+        </div>
+        <p>{{ calendarTitle }}</p>
+        <a
+          :is="calendarUrl ? 'a' : 'div'"
+          :href="calendarUrl"
+          :title="calendarText"
+        >
+          {{ calendarText }}
+        </a>
+      </li>
+
+      <li v-if="locationTitle || locationText">
+        <div class="osg-icon osg-v-default osg-u-icon-location-pin">
+          <span class="osg-sr-only">Location</span>
+        </div>
+        <p>{{ locationTitle }}</p>
+        <a
+          :is="locationUrl ? 'a' : 'div'"
+          :href="locationUrl"
+        >
+          {{ locationText }}
+        </a>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "OsgInfoList",
+
+    props: {
+      calendarTitle: {
+        type: String,
+        required: true,
+      },
+
+      calendarText: {
+        type: String,
+      },
+
+      calendarUrl: {
+        type: String,
+      },
+      
+      locationTitle: {
+        type: String,
+        required: true,
+      },
+
+      locationText: {
+        type: String,
+      },
+
+      locationUrlText: {
+        type: String,
+      },
+
+      locationUrl: {
+        type: String,
+      }
+    },
+  }
+  
+</script>
+
+<style lang="sass">
+  @import "~styleguide/src/assets/sass/resources.sass"
+  @import "~styleguide/src/assets/sass/common/utils"
+  @import "~styleguide/src/molecules/data_display/info_list/info_list.sass"
+</style>
+
+
+
---
+++ src/organisms/headings/event/event.vue
@@ -0,0 +1,140 @@
+<template>
+<div
+    class="osg-event"
+    :class="[
+      { 'osg-event--with-state': stateText },
+    ]"
+  >
+  <div class="osg-event__header">
+    <h2>
+      {{ title }}
+    </h2>
+  </div>
+  <div class="osg-event__image">
+    <div v-if="imageUrl" class="osg-event__image-container">
+        <osg-vue-figure
+          :url="imageUrl"
+          :url-mobile="imageUrlMobile"
+          :url-tablet="imageUrlTablet"
+          :url-desktop="imageUrlDesktop"
+          :sr-description="imageSrDescription"
+        />
+
+        <div
+          v-if="stateText"
+          class="osg-event__state-text"
+        >
+          {{ stateText }}
+        </div>
+
+    </div>
+    <div class="osg-event__info">
+      <osg-vue-info-list
+        :calendarTitle="calendarTitle"
+        :calendarText="calendarText"
+        :calendarUrl="calendarUrl"
+        :locationTitle="locationTitle"
+        :locationText="locationText"
+        :locationUrl="locationUrl"
+      />
+
+      <div
+        v-if="imageCaption"
+        class="osg-event__caption"
+      >
+        {{ imageCaption }}
+      </div>
+
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+  import OsgVueFigure from '../../../atoms/decorators/figure/figure.vue'
+  import OsgVueInfoList from '../../../molecules/data_display/info_list/info_list.vue'
+
+  export default {
+    name: "OsgEvent",
+
+    components: {
+      OsgVueFigure,
+      OsgVueInfoList,
+    },
+
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+
+      imageUrl: {
+        type: String,
+        required: true,
+      },
+
+      imageCaption: {
+        type: String,
+      },
+
+      imageUrlMobile: {
+        type: String
+      },
+
+      imageUrlTablet: {
+        type: String
+      },
+
+      imageUrlDesktop: {
+        type: String
+      },
+
+      imageSrDescription: {
+        type: String
+      },
+
+      stateText: {
+        type: String,
+      },
+
+      calendarTitle: {
+        type: String,
+        required: true,
+      },
+
+      calendarText: {
+        type: String,
+      },
+
+      calendarUrl: {
+        type: String,
+      },
+      
+      locationTitle: {
+        type: String,
+        required: true,
+      },
+
+      locationText: {
+        type: String,
+      },
+
+      locationUrlText: {
+        type: String,
+      },
+
+      locationUrl: {
+        type: String,
+      }
+    }
+  }
+</script>
+
+<style lang="sass">
+  @import "~styleguide/src/assets/sass/resources.sass"
+  @import "~styleguide/src/assets/sass/common/_utils.sass"
+  @import "~styleguide/src/organisms/headings/event/event.sass"
+</style>
+
+
+
