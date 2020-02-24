3c76787e58ab8ae8f2c1c3fd34b28f0f32d53c4f
Kristoffer Solheim <kris.sunhome@gmail.com>
2020-02-24 10:08:59 +0100

    added checkbox vue component

---
+++ src/atoms/checkbox/checkbox.vue
@@ -0,0 +1,34 @@
+<template>
+  <label class="osg-checkbox osg-u-text-5">
+    <input type="checkbox"
+      :disabled="disabled"
+      :checked="checked"
+      v-bind="dataAttrs"
+    >
+    <span class="osg-checkbox__checkmark"></span
+    ><span class="osg-u-padding-left-3 osg-checkbox__text">
+      <slot />
+    </span>
+  </label>
+</template>
+
+<script>
+  export default {
+    name: 'OsgVueCheckbox',
+
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      checked: {
+        type: Boolean,
+        default: false
+      },
+      dataAttrs: {
+        type: Object,
+        default: () => ({})
+      }
+    }
+  }
+</script>
