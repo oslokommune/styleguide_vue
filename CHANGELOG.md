347c45d4e9bb0e609be1e842b9e3ec0c61cf994f
Kristoffer Solheim <kris.sunhome@gmail.com>
2020-02-27 10:05:10 +0100

    added dropdown and changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- atoms-dropdown: added single-select dropdown component.
+
 ### Removed
 
 ### Changed
---
+++ src/atoms/dropdown/dropdown.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="osg-dropdownfield">
+    <select id="id" class="osg-u-text-5 osg-form-component select-form">
+      <option
+        v-for="selectOption in selectOptions" 
+        :key="selectOption.value"
+        value="selectOption.value"
+      >
+        {{selectOption.name}}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'OsgVueDropdown',
+    props: {
+      id: {
+        type: String,
+        default: 'dropdown-select'
+      },
+      selectOptions: {
+        type: Array
+      }
+    }
+  }
+</script>
