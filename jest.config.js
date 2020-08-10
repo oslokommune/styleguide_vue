5abb050926af2b31b90155557ca190549d27cf29
Kristoffer Nordström <totte.nordstrom@gmail.com>
2020-08-10 10:44:28 +0200

    Adds unit test suite

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "moduleFileExtensions": [
+    "js",
+    "jsx",
+    "json",
+    "vue"
+  ],
+  "transform": {
+    "^.+\\.vue$": "vue-jest",
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  "transformIgnorePatterns": [
+    "/node_modules/"
+  ],
+  "moduleNameMapper": {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  "snapshotSerializers": [
+    "jest-serializer-vue"
+  ],
+  "testMatch": [
+    "**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
+  ],
+  "testURL": "http://localhost/",
+  "collectCoverage": true,
+  "collectCoverageFrom": [
+    "**/*.{js,vue}",
+    "!**/node_modules/**",
+    "!**/tests/**",
+    "!**/*.{config,spec}.js"
+  ],
+  "coverageReporters": [
+    "text-summary"
+  ]
+}
